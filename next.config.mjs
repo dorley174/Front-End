2603c004e5018e69d7983a5edf98c932d450fe08
dorley174 <67094121+dorley174@users.noreply.github.com>
2024-07-06 23:02:06 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    assetPrefix: isProd ? '/frontend/' : '',
-    basePath: isProd ? '/frontend' : '',
+    assetPrefix: isProd ? '/Front-End/' : '',
+    basePath: isProd ? '/Front-End' : '',
     trailingSlash: true,
     reactStrictMode: true,
     webpack: (config) => {
